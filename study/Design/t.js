756ae71a8b0ca55015e9efa68c7aeb6288886fa1
juhongchul <jhc9639@naver.com>
2019-04-07 21:36:59 +0900

    면접준비용 수정

--- study/Design/t.js
+++
@@ -1,6 +0,0 @@
-var express = require('express')
-var app = exppress();
-app.get('/', (req, res)=>{
-    res.send("{a:b")
-})
-app.listen(3000)
--- study/Design/해시테이블.js
+++
@@ -1,6 +0,0 @@
-var myMap = new Map(); 
-var k = '1';
-// setting the values
-myMap.set(k, "kundol is best"); 
-myMap.size; // 3 
-console.log(myMap.get(k)); 
