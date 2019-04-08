af4f2a061fb0361b3d5ee4f6d322f2142308a8c5
juhongchul <jhc9639@naver.com>
2019-04-09 07:40:50 +0900

    add 4.09

--- 면접준비/test/test.js
+++ 면접준비/test/test.js
@@ -1,15 +1,7 @@
-var arr = [1, 2, 3, 4]
-function a(){
-    function b(val){
-        i = 10
-        console.log(i)
-        return val * i
-    }
-    for(var i = 0; i < arr.length; i++){
-        arr[i] = b(arr[i])
-    }
-    return arr; 
-}
-console.log(a())
-//10
-//[ 10, 2, 3, 4 ]
+function constant$1(x) {
+    return function() {
+      return x;
+    };
+  }
+const ret = constant$1(1)
+console.log(ret.call()); 
